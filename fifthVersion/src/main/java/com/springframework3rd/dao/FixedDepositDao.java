353b0044b0466ec1196fd4c2f3ce48c94f7f91ba
olyver <demar.07@gmail.com>
2016-12-21 21:13:14 +0900

    Has been implemented BeanFactoryPostProcessor.

--- fifthVersion/src/main/java/com/springframework3rd/dao/FixedDepositDao.java
+++ fifthVersion/src/main/java/com/springframework3rd/dao/FixedDepositDao.java
@@ -1,6 +1,7 @@
 package com.springframework3rd.dao;
 
 import com.springframework3rd.common.IInstanceValidator;
+import com.springframework3rd.domain.IFixedDepositDetails;
 import org.apache.log4j.Logger;
 
 import javax.annotation.PostConstruct;
@@ -8,8 +9,13 @@ import javax.annotation.PostConstruct;
 public class FixedDepositDao implements IFixedDepositDao, IInstanceValidator {
     private static Logger logger = Logger.getLogger(FixedDepositDao.class);
     private DatabaseConnection connection;
+    private IFixedDepositDetails fixedDepositDetails;
     public FixedDepositDao() {
         logger.info("FixedDepositDao constructor invoked.");
+    }
+
+    public void setFixedDepositDetails(IFixedDepositDetails fixedDepositDetails) {
+        this.fixedDepositDetails = fixedDepositDetails;
     }
 
     @PostConstruct
